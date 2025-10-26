0703f38d6bf4c9e0d46a6c4361866bcdca1dac68
CharlieDan01 <danielzc79@gmail.com>
2025-10-26 13:22:51 -0600

    Primer commit - Portafolio personal

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// Obtener el año actual y colocarlo en el elemento del footer
+document.addEventListener("DOMContentLoaded", () => {
+  const yearSpan = document.getElementById("currentYear");
+  if (yearSpan) {
+    yearSpan.textContent = new Date().getFullYear();
+  }
+});
