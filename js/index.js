f0e6da6e8926bac33d61f4c3fe07da7114ca14fe
CharlieDan01 <danielzc79@gmail.com>
2025-10-28 21:39:59 -0600

    remove the dark theme, isn't work

--- js/index.js
+++ js/index.js
@@ -5,22 +5,4 @@ document.addEventListener("DOMContentLoaded", () => {
     yearSpan.textContent = new Date().getFullYear();
   }
 });
-// Cambiar entre modo claro y oscuro
-const switchButton = document.getElementById("switch");
-const body = document.body;
 
-// Verifica si el usuario ya tenía un modo guardado
-if (localStorage.getItem("theme") === "dark") {
-  body.classList.add("dark-mode");
-  switchButton.checked = true;
-}
-
-switchButton.addEventListener("change", () => {
-  if (switchButton.checked) {
-    body.classList.add("dark-mode");
-    localStorage.setItem("theme", "dark");
-  } else {
-    body.classList.remove("dark-mode");
-    localStorage.setItem("theme", "light");
-  }
-});
