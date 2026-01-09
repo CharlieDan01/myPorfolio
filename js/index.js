78a1c2eb1162ae8391b46435597871afc84cd1c4
CharlieDan01 <danielzc79@gmail.com>
2026-01-08 21:18:41 -0600

    Implementación de proyectos dinámicos con GitHub API

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 // Obtener el año actual y colocarlo en el elemento del footer
 document.addEventListener("DOMContentLoaded", () => {
-  const yearSpan = document.getElementById("currentYear");
-  if (yearSpan) {
+    const yearSpan = document.getElementById("currentYear");
+    if (yearSpan) {
     yearSpan.textContent = new Date().getFullYear();
-  }
+    }
 });
 
 // Diccionario de traducciones
